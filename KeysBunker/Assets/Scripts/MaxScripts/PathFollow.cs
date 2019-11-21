92056d3736a5e19c5a14f174bc263d2beede076d
Maxence <maxdurlon@gmail.com>
2019-11-21 17:29:33 +0100

    Test Nodes Movement

---
+++ KeysBunker/Assets/Scripts/MaxScripts/PathFollow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class PathFollow : MonoBehaviour
+{
+    private Transform[] nodes;
+    public Transform missile;
+    private Vector3 targetedPosition;
+    private Vector3 startPosition;
+    public float movingSpeed;
+    private int currentNode;
+    private float timer;
+
+    void Start()
+    {
+        nodes = GetComponentsInChildren<Transform>();
+
+        Check();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime * movingSpeed;
+
+        if (missile.transform.position != targetedPosition)
+        {
+            missile.transform.position = Vector3.Lerp(startPosition, targetedPosition, timer);
+        }
+
+        else
+        {
+            if (currentNode < nodes.Length - 1)
+            {
+                currentNode++;
+                Check();
+            }
+        }
+    }
+
+    void Check()
+    {
+        timer = 0;
+        targetedPosition = nodes[currentNode].transform.position;
+        startPosition = missile.transform.position;
+    }
+}
