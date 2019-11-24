21b0396187fbfd817a4eee953b67974a4d89afdf
Maxence <maxdurlon@gmail.com>
2019-11-24 21:14:37 +0100

    Zonklord 69

---
+++ KeysBunker/Assets/TrailerMissilesSpawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerMissilesSpawner : MonoBehaviour
+{
+    public List<GameObject> missiles;
+    public List<GameObject> paths;
+
+    private List<int> index =  new List<int>();
+
+    private int currentIndex = 0;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (index != null)
+            {
+                foreach (int ind in index)
+                {
+                    if (ind == currentIndex)
+                    {
+                        if (ind >= 3) return;
+                        else currentIndex++;
+                    }
+                }
+            }
+
+            GameObject currentPath = Instantiate(paths[currentIndex], gameObject.transform.position, Quaternion.identity);
+
+            GameObject currentMissile = Instantiate(missiles[Random.Range(0, missiles.Count)], currentPath.GetComponentInChildren<PathFollow>().transform.position, Quaternion.identity);
+
+            currentPath.GetComponentInChildren<PathFollow>().missile = currentMissile.transform;
+
+            index.Add(currentIndex);
+        }
+    }
+}
