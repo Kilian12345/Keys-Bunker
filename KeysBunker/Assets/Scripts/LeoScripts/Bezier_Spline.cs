969a78e97a95c4163e5cb90fed91a9807d3ed280
Léonard <epoc4oftime@gmail.com>
2020-01-10 16:34:34 +0100

    FT_Implemented Custom Spline Creation and Tracking
    developing procedural spline generation

---
+++ KeysBunker/Assets/Scripts/LeoScripts/Bezier_Spline.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System;
+
+//[ExecuteInEditMode, RequireComponent(typeof(LineRenderer))]
+public class Bezier_Spline : MonoBehaviour
+{
+    [SerializeField] Color color = Color.white;
+    [SerializeField] float width = 0.2f;
+    [SerializeField] int numberOfPoints = 20;
+    LineRenderer lineRenderer;
+    [SerializeField] GameObject controlPoint;
+    [SerializeField] GameObject missile;
+    [SerializeField] Vector3[] splinePositions;
+    [SerializeField] List<GameObject> controlPoints = new List<GameObject>();
+    [ShowInInspector] Queue<GameObject> nodeQueue = new Queue<GameObject>();
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.useWorldSpace = true;
+        lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Additive"));
+
+        splinePositions = new Vector3[lineRenderer.positionCount];
+        lineRenderer.GetPositions(splinePositions);
+
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            GameObject node = Instantiate(controlPoint, lineRenderer.GetPosition(i), Quaternion.identity);
+            nodeQueue.Enqueue(node);
+            node.transform.parent = gameObject.transform;
+        }
+
+        missile.gameObject.SendMessage("ReceiveNodeQueue", nodeQueue);
+    }
+
+
+    void Update()
+    {
+        GenerateSpline();
+
+        CheckMissileStatus();
+    }
+
+    private void CheckMissileStatus()
+    {
+        if(missile == null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void GenerateSpline()
+    {
+        if (null == lineRenderer || controlPoints == null || controlPoints.Count < 3)
+        {
+            return; // not enough points specified
+        }
+        // update line renderer
+        lineRenderer.startColor = color;
+        lineRenderer.endColor = color;
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+
+        if (numberOfPoints < 2)
+        {
+            numberOfPoints = 2;
+        }
+        lineRenderer.positionCount = numberOfPoints * (controlPoints.Count - 2);
+
+        Vector3 p0, p1, p2;
+        for (int j = 0; j < controlPoints.Count - 2; j++)
+        {
+            // check control points
+            if (controlPoints[j] == null || controlPoints[j + 1] == null
+            || controlPoints[j + 2] == null)
+            {
+                return;
+            }
+            // determine control points of segment
+            p0 = 0.5f * (controlPoints[j].transform.position
+            + controlPoints[j + 1].transform.position);
+            p1 = controlPoints[j + 1].transform.position;
+            p2 = 0.5f * (controlPoints[j + 1].transform.position
+            + controlPoints[j + 2].transform.position);
+
+            // set points of quadratic Bezier curve
+            Vector3 position;
+            float t;
+            float pointStep = 1.0f / numberOfPoints;
+            if (j == controlPoints.Count - 3)
+            {
+                pointStep = 1.0f / (numberOfPoints - 1.0f);
+                // last point of last segment should reach p2
+            }
+            for (int i = 0; i < numberOfPoints; i++)
+            {
+                t = i * pointStep;
+                position = (1.0f - t) * (1.0f - t) * p0
+                + 2.0f * (1.0f - t) * t * p1 + t * t * p2;
+                lineRenderer.SetPosition(i + j * numberOfPoints, position);
+            }
+        }
+    }
+}
---
+++ KeysBunker/Assets/Scripts/LeoScripts/SplineMissile.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+using System;
+
+public class SplineMissile : MonoBehaviour
+{
+    //TWEEN VALUES
+    [SerializeField] float time;
+    [SerializeField] Vector3 change;
+    [SerializeField] Vector3 startPosition;
+    [SerializeField] Vector3 targetPosition;
+    [SerializeField] float duration;
+
+    [ShowInInspector] Queue<GameObject> receivedNodeQueue;
+
+    public float xCoord;
+    public float yCoord;
+    public float xFrequency;
+    public float yFrequency;
+
+    Vector2 prevMaxInterval = new Vector2(2,1);
+    Vector2 prevMinInterval = new Vector2(-2,-1);
+
+    Vector2 nextMaxInterval = new Vector2(1.75f, .5f);
+    Vector2 nextMinInterval = new Vector2(-1.75f, -.5f);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector2 RandomSelectedCoords = new Vector2(UnityEngine.Random.Range(UnityEngine.Random.Range(prevMaxInterval.x, nextMaxInterval.x), 
+                                                                            UnityEngine.Random.Range(prevMaxInterval.y, nextMaxInterval.y)), 
+                                                    UnityEngine.Random.Range(UnityEngine.Random.Range(prevMinInterval.x, nextMinInterval.x), 
+                                                                             UnityEngine.Random.Range(prevMinInterval.y, nextMinInterval.y)));
+
+        Debug.Log(RandomSelectedCoords);
+        
+        Vector2 determinator = new Vector2(UnityEngine.Random.Range(0, 1), UnityEngine.Random.Range(0, 1));
+
+        if(determinator == new Vector2 (0, 0))
+        {
+
+        }
+        else if(determinator == new Vector2(0, 1))
+        {
+
+        }
+
+        else if(determinator == new Vector2(1, 0))
+        {
+
+        }
+        else if (determinator == new Vector2(1, 1))
+        {
+
+        }
+    }
+
+    void Update()
+    {
+        MissileMovement();
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(Mathf.PerlinNoise(xCoord * xFrequency * Time.time, yCoord * yFrequency * Time.deltaTime));
+        }
+    }
+
+    private void MissileMovement()
+    {
+        change = targetPosition - startPosition;
+
+        if (time <= duration)
+        {
+            time += Time.deltaTime;
+            transform.position = new Vector2(TweenManager.LinearTween(time, startPosition.x, change.x, duration), TweenManager.LinearTween(time, startPosition.y, change.y, duration));
+        }
+
+        if (time >= duration)
+        {
+            receivedNodeQueue.Dequeue();
+            targetPosition = receivedNodeQueue.Peek().transform.position;
+            startPosition = transform.position;
+            time = 0f;
+        }
+    }
+
+    void ReceiveNodeQueue(Queue<GameObject> queue)
+    {
+        receivedNodeQueue = queue;
+        Debug.Log("Message Received");
+        startPosition = transform.position;
+        targetPosition = receivedNodeQueue.Peek().transform.position;
+    }
+}
