f1a79620601b74d6af18c317e1a7c3fd3f017d9a
Maxence <maxdurlon@gmail.com>
2020-01-16 11:18:07 +0100

    Rangement bien spasser

---
+++ KeysBunker/Assets/Scripts/MaxScripts/Else/Outdated/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject targetedObject;
+    public float offset;
+
+    void Update()
+    {
+        gameObject.transform.position = new Vector3(targetedObject.transform.position.x, targetedObject.transform.position.y, offset);
+    }
+}
---
+++ KeysBunker/Assets/Scripts/MaxScripts/HQHealthManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HQHealthManager : MonoBehaviour
+{
+    public int baseHealth;
+    private int health;
+
+    void Start()
+    {
+        health = baseHealth;
+    }
+
+    void Update()
+    { 
+        if (health <= 0)
+        {
+            string currentScene = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+
+    void Hit()
+    {
+        health--;
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (!col.GetComponent<ExplosionUFO>().hasHit)
+        {
+            Hit();
+            Debug.Log("hit !" + health);
+            Debug.Log(col.transform.gameObject);
+            col.GetComponent<ExplosionUFO>().hasHit = true;
+        }
+    }
+}
